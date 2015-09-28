f65c930e8aaee99fc7a6c1c1994d51ea4abc3fb8
jtrevag <jtrevag@gmail.com>
2015-09-28 15:34:17 +0000

    Added routing for writing to DB

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,30 @@ router.get('/newuser', function(req, res) {
     res.render('newuser', { title: 'Add New User' });
 });
 
+router.post('/adduser', function(req,res) {
+    // Set our internal DB variable
+    var db = req.db;
+    
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    
+    // Set our collection
+    var collection = db.get('usercollection');
+    
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email"    : userEmail,
+    }, function(err, doc){
+        if(err) {
+            // If it failed, return error
+            res.send("There was a problema dding the information to the database.");
+        }
+        else {
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
