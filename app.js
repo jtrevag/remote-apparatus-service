edd85afa13d2c700b067d5e33743aae4fbd623b5
jtrevag <jtrevag@gmail.com>
2015-10-07 04:23:05 +0000

    Added light service to interact with raspberry pi

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+var lightServiceApp = express();
+var lightService = require('./routes/lightservice.js');
+lightServiceApp.use('/', lightService);
+
+app.use('/lightService', lightServiceApp);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ routes/lightservice.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) { sys.puts(stdout); };
+
+router.get('/computer_room/on', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4281795", puts);
+    // Return JSON(?)
+});
+
+router.get('/computer_room/off', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4281804", puts);
+    // Return JSON(?)
+});
+
+router.get('/bedroom/on', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4282115", puts);
+    // Return JSON(?)
+});
+
+router.get('/bedroom/off', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4282124", puts);
+    // Return JSON(?)
+});
+
+router.get('/living_room/front/on', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4283651", puts);
+    // Return JSON(?)
+});
+
+router.get('/living_room/front/off', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4283660", puts);
+    // Return JSON(?)
+});
+
+router.get('/living_room/back/on', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4289795", puts);
+    // Return JSON(?)
+});
+
+router.get('/living_room/back/off', function(req, res) {
+    // Execute codesend
+    exec("/var/www/rfoutlet/codesend 4289804", puts);
+    // Return JSON(?)
+});
