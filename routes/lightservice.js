c6749b752aca1e658583a0086dc0a89cdb2f0fe7
jtrevag <jtrevag@gmail.com>
2015-10-14 02:42:28 +0000

    Setting up the light service to call several pulses

--- routes/lightservice.js
+++ routes/lightservice.js
@@ -5,25 +5,29 @@ var exec = require('child_process').exec;
 
 function puts(error, stdout, stderr) { sys.puts(stdout); };
 
+function sendLightCode(lightCode){
+    for(var x = 183; x < 186; x++){
+        exec("/var/www/rfoutlet/codesend " + lightCode + " " + x, puts);    
+    }
+}
 
 router.route('/')
 .put(function(req,res,next){
     var state = req.body.state;
     var roomName = req.body.room;
-    var check = req.body.check;
     var db = req.db;
     var collection = db.get('light_collection');
     
     //TODO: test and see if this garbage works.
     if(state === 'on' && roomName != null){
         collection.find({ room: roomName },{},function(e,docs){
-            exec("/var/www/rfoutlet/codesend " + docs[0]["on_code"], puts);
+            sendLightCode(docs[0]["on_code"]);
             res.json({ message: 'Light turned on for room: ' + docs[0]["room"] + '!' });
         });
     }
     else if(state === 'off'){
          collection.find({ room: roomName },{},function(e,docs){
-            exec("/var/www/rfoutlet/codesend " + docs[0]["off_code"], puts);
+            sendLightCode(docs[0]["off_code"]);
             res.json({ message: 'Light turned off for room: ' + docs[0]["room"] + '!' });
         });
     }
